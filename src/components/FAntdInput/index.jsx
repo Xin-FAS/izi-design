096732b6dc6b5100ea793b5cb09fc03d15993d8d
xin-fas <xin.fas@outlook.com>
2025-04-03 16:02:51 +0800

    feat(FAntdInput): 添加FAntdInput.Number组件

--- src/components/FAntdInput/index.jsx
+++ src/components/FAntdInput/index.jsx
@@ -1,6 +1,6 @@
-import { Input } from 'antd';
-import { forwardRef, useCallback } from 'react';
-import { throttle, debounce } from 'lodash-es'
+import { Input, InputNumber } from 'antd';
+import { forwardRef, useCallback, useMemo } from 'react';
+import { throttle, debounce } from 'lodash-es';
 
 // 获取公共属性
 const getBaseArgs = ({
@@ -12,52 +12,52 @@ const getBaseArgs = ({
     allowClear: true,
     value: state?.[0],
     onChange: (event, ...args) => {
-        state?.[1](event.target.value, ...args)
-        onChange && onChange(event, ...args)
+        state?.[1](event.target.value?.trim(), ...args);
+        onChange && onChange(event, ...args);
     },
-    ...args
-})
+    ...args,
+});
 
 // 基础Input
 const FAntdInput = forwardRef((props, ref) => <Input
     placeholder={'请输入'}
     {...getBaseArgs(props, ref)}
-/>)
+/>);
 
 // 先将其他子组件也带过来
-const { $$typeof, render, ...components } = Input
-for (const name in components) FAntdInput[name] = Input[name]
+const { $$typeof, render, ...components } = Input;
+for (const name in components) FAntdInput[name] = Input[name];
 
 // 重写Password
 FAntdInput.Password = forwardRef((props, ref) =>
     <Input.Password
         placeholder={'请输入密码'}
         {...getBaseArgs(props, ref)}
-    />
-)
+    />,
+);
 // 重写Search
 FAntdInput.Search = forwardRef((props, ref) => {
     // 搜索
-    const toSearch = (...args) => {
-        props.onSearch && props.onSearch(...args)
-    }
+    const onSearch = (...args) => {
+        props.onSearch && props.onSearch(...args);
+    };
     // 节流搜索
-    const throttleSearch = throttle(toSearch, props.throttleDuration ?? 0, { trailing: props.throttleTrailing ?? false })
+    const throttleSearch = throttle(onSearch, props.throttleDuration ?? 0, { trailing: props.throttleTrailing ?? false });
     // 自动搜索
-    const debounceSearch = useCallback(debounce(toSearch, props.debounceDuration ?? 500), [])
+    const debounceSearch = useCallback(debounce(onSearch, props.debounceDuration ?? 500), []);
     // 输入
     const onInput = event => {
-        if (props.autoSearch) debounceSearch(event.target.value)
-        props.onInput && props.onInput(event)
-    }
+        if (props.autoSearch) debounceSearch(event.target.value);
+        props.onInput && props.onInput(event);
+    };
 
     return <Input.Search
-        placeholder={props.autoSearch ? '输入后自动搜索': '请输入'}
+        placeholder={props.autoSearch ? '输入后自动搜索' : '请输入'}
         {...getBaseArgs(props, ref)}
         onSearch={throttleSearch}
         onInput={onInput}
-    />
-})
+    />;
+});
 // 重写TextArea
 FAntdInput.TextArea = forwardRef((props, ref) =>
     <Input.TextArea
@@ -66,22 +66,75 @@ FAntdInput.TextArea = forwardRef((props, ref) =>
         maxLength={100}
         {...getBaseArgs(props, ref)}
         style={{ height: 100, ...props?.style }}
-    />
-)
+    />);
 // 重写OTP
 FAntdInput.OTP = forwardRef(({
     state,
     onInput,
     ...args
-}, ref) => <Input.OTP
+}, ref) =>
+    <Input.OTP
         ref={ref}
         value={state?.[0]}
         {...args}
         onInput={value => {
             state?.[1](value.join(''));
-            onInput && onInput(value)
+            onInput && onInput(value);
         }}
-    />
-)
+    />);
 
-export default FAntdInput
+// 添加Number模式
+FAntdInput.Number = forwardRef(({
+    state,
+    onChange,
+    mode,
+    strict, // 严格模式（限制步长为1的正整数）
+    ...args
+}, ref) => {
+    const getStrictValue = value => strict ? ~~value: value
+    const modeArgs = useMemo(() => {
+        let args = {}
+        // 价格格式
+        if (mode === 'price') {
+            args = {
+                formatter: value => `￥${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+                parser: value => getStrictValue(value?.replace(/\￥\s?|(,*)/g, ''))
+            }
+        }
+        // 百分比格式
+        if (mode === 'percent') {
+            args = {
+                formatter: value => `${value}%`,
+                parser: value => getStrictValue(value?.replace('%', ''))
+            }
+        }
+        return args
+    }, [])
+
+    const useMax = useMemo(() => {
+        if (mode === 'percent' && strict) return 100
+        return undefined
+    }, [])
+
+    const useOnChange = value => {
+        state?.[1](value);
+        onChange && onChange(value);
+    }
+
+    return <InputNumber
+        placeholder={'请输入'}
+        ref={ref}
+        value={state?.[0]}
+        keyboard
+        changeOnWheel
+        min={strict ? 0: undefined}
+        max={useMax}
+        step={strict ? 1: 0.1}
+        precision={strict ? 0 : undefined}
+        {...modeArgs}
+        {...args}
+        onChange={useOnChange}
+    />;
+});
+
+export default FAntdInput;
