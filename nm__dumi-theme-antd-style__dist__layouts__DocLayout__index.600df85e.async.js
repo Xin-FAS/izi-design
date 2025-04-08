315f60fb2a7d9ec563a602c292e928217ad52cbe
Xin-FAS <127968300+Xin-FAS@users.noreply.github.com>
2025-04-08 10:40:44 +0000

    Deploying to gh-pages from @ Xin-FAS/izi-design@d42628300e15e0d6f7d2f9012997bb36bca805d7 🚀

---
+++ nm__dumi-theme-antd-style__dist__layouts__DocLayout__index.600df85e.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkizid=self.webpackChunkizid||[]).push([[860],{72007:function(n,e,_){var t=_(70623),i=_(85893);e.Z=function(){return(0,i.jsx)(t.Z,{bottom:"\xA9 2025 izi-design"})}}}]);
